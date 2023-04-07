f6ea4712ed1c4bc6fb7c1961448025364e8052d0
ProToTime3560 <prototime3560@naver.com>
2023-04-08 07:44:20 +0900

    third commit(urlparameter)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,65 @@
 import React, { useState } from 'react';
 import { Button, Navbar, Container, Nav, useAccordionButton } from 'react-bootstrap';
 import './App.css';
-import {data, Card} from './data.js';
-import { Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
-import  Detail  from './routes/Detailpage.js'
-import  Event from './routes/Eventpage.js'
-import  About from './routes/Aboutpage.js'
+import { data, Card } from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detailpage.js'
+import Event from './routes/Eventpage.js'
+import About from './routes/Aboutpage.js'
 
 function App() {
 
-  let [item] = useState(data)
+  let [item, setitem] = useState(data)
   let navigate = useNavigate();
 
-  return ( 
-    <div className="App"> 
-
-       <Navbar bg="light" variant="light">
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">프로토Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={ () => { navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={ () => { navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail/0') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
+
+      <Button onClick={() => {
+        let newArray = [...item];
+        newArray.sort((a, b) =>
+          a.title.localeCompare(b.title));
+        setitem(newArray);
+      }}>상품정렬</Button>
       <Routes>
-        <Route path="/" element= { 
+        <Route path="/" element={
           <>
-          <div className="main-bg"></div>
-          <div className="container">
+            <div className="main-bg"></div>
+            <div className="container">
               <div className="row">
-               {
-                item.map(function(a, i) {
-                  return (
-                    <Card item={item[i]}></Card>
-                  )
-                })
-               }
+                {
+                  item.map(function (a, i) {
+                    return (
+                      <Card item={item[i]}></Card>
+                    )
+                  })
+                }
               </div>
-           </div>
+            </div>
           </>
         } />
-        <Route path="/detail" element= { <Detail/>} />
-        <Route path="*" element= {<div>404 not found.</div> } />
-        <Route path="/about" element={ <About/>}>
-          <Route path="member" element= {<div>멤버임</div>}/>
-          <Route path="location" element= {<div>위치임</div>}/>
+        <Route path="/detail/:userinputid" element={<Detail item={item} />} />
+        <Route path="*" element={<div>404 not found.</div>} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치임</div>} />
         </Route>
-        <Route path="/event" element={<Event/>}>
-          <Route path="one" element= { <div>첫 주문시 양배추즙 서비스</div>}></Route>
-          <Route path="two" element= { <div>생일기념 쿠폰받기</div>}></Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
         </Route>
 
       </Routes>
-       
+
     </div>
   );
 }
--- src/routes/Detailpage.js
+++ src/routes/Detailpage.js
@@ -1,19 +1,30 @@
-function Detail() {
+import { useParams } from 'react-router-dom'
+function Detail(props) {
+
+  let { userinputid } = useParams();
+  if (userinputid <= props.item.length) {
+    let originproduct = props.item.find(item => item.id == userinputid);
     return (
-        <div className="container">
+      <div className="container">
         <div className="row">
-            <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6">
-      <h4 className="pt-5">상품명</h4>
-      <p>상품설명</p>
-      <p>120000원</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
+          <div className="col-md-6">
+            <img src={process.env.PUBLIC_URL + originproduct.imgUrl} className="item-img"></img>
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{originproduct.title}</h4>
+            <p>{originproduct.content}</p>
+            <p>{originproduct.price}</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
+        </div>
+      </div>
     )
+  }
+  return (
+    <div> 404 not found.</div>
+  )
 }
+
+
 
 export default Detail;
